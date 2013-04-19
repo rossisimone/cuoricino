2384cd1326cc9b6930fe990550d9d69ccf472370
Giacomo Rosilho de Souza <giacomo.rosilhodesouza@epfl.ch>
2013-04-19 11:04:36 +0200

    members in .cpp Conflicts:
    	lifev/core/fem/CMakeLists.txt

--- lifev/core/fem/ROS3P.cpp
+++ lifev/core/fem/ROS3P.cpp
@@ -34,24 +34,32 @@
     @date 01-04-2013
  */
 
-
-#ifndef ROS3P_HPP_
-#define ROS3P_HPP_
-
-#include <lifev/core/fem/RosenbrockTransformed.hpp>
+#include <lifev/core/fem/ROS3P.hpp>
 
 
 namespace LifeV
 {
 
-class ROS3P : public RosenbrockTransformed<3>
+ROS3P::ROS3P()
+: RosenbrockTransformed<3>()
 {
-public:
-	ROS3P();
-};
+	MatrixSmall<3,3> A;
+	MatrixSmall<3,3> C;
+	VectorSmall<3> gammai;
+	VectorSmall<3> a;
+	VectorSmall<3> m;
+	VectorSmall<3> mhat;
+	Real g(0.7886751345948129);
 
-} //namesapce LifeV
+	A(1,0) = 1.267949192431123;			A(2,0) = 1.267949192431123;
+	C(1,0) = -1.607695154586736;		C(2,0) = -3.464101615137755;		C(2,1) = -1.732050807568877;
+	gammai(0) = 0.7886751345948129;		gammai(1) = -0.2113248654051871;	gammai(2) = -1.077350269189626;
+	a(0) = 0.0;							a(1) = 1.0;							a(2) = 1.0;
+	m(0) = 2.0;							m(1) = 0.5773502691896258;			m(2) = 0.4226497308103742;
+	mhat(0) = 2.113248654051871;		mhat(1) = 1.0;						mhat(2) = 0.4226497308103742;
 
+	this->setMethod(g, A, C, gammai, a, m, mhat, 3);
+	this->initMembers();
+}
 
-
-#endif /* ROS3P_HPP_ */
+}// namespace LifeV
