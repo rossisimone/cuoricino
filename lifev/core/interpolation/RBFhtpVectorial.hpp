335e7fce4d1bcad1d7a4b1502e87f610e67ef48b
Simone Rossi <simone.rossi.epfl.ch>
2013-05-02 00:48:54 +0200

    adding std in the htp interpolation

--- lifev/core/interpolation/RBFhtpVectorial.hpp
+++ lifev/core/interpolation/RBFhtpVectorial.hpp
@@ -384,9 +384,9 @@ void RBFhtpVectorial<mesh_Type>::projectionOperator()
         {
             if ( M_flags[0] == -1 || this->isInside (M_fullMeshKnown->point (j).markerID(), M_flags) )
             {
-                d = std::sqrt ( pow (M_fullMeshKnown->point (j).x() - M_fullMeshUnknown->point (GlobalID[k]).x(), 2)
-                                + pow (M_fullMeshKnown->point (j).y() - M_fullMeshUnknown->point (GlobalID[k]).y(), 2)
-                                + pow (M_fullMeshKnown->point (j).z() - M_fullMeshUnknown->point (GlobalID[k]).z(), 2) );
+                d = std::sqrt ( std::pow (M_fullMeshKnown->point (j).x() - M_fullMeshUnknown->point (GlobalID[k]).x(), 2)
+                                + std::pow (M_fullMeshKnown->point (j).y() - M_fullMeshUnknown->point (GlobalID[k]).y(), 2)
+                                + std::pow (M_fullMeshKnown->point (j).z() - M_fullMeshUnknown->point (GlobalID[k]).z(), 2) );
                 if (d < d_min)
                 {
                     d_min = d;
@@ -442,9 +442,9 @@ double RBFhtpVectorial<mesh_Type>::computeRBFradius (meshPtr_Type MeshNeighbors,
     double r_max = 0;
     for (idContainer_Type::iterator it = Neighbors.begin(); it != Neighbors.end(); ++it)
     {
-        r = std::sqrt ( pow ( MeshGID->point ( GlobalID ).x() - MeshNeighbors->point ( *it ).x(), 2 )
-                        + pow ( MeshGID->point ( GlobalID ).y() - MeshNeighbors->point ( *it ).y(), 2 )
-                        + pow ( MeshGID->point ( GlobalID ).z() - MeshNeighbors->point ( *it ).z(), 2 ) );
+        r = std::sqrt ( std::pow ( MeshGID->point ( GlobalID ).x() - MeshNeighbors->point ( *it ).x(), 2 )
+                        + std::pow ( MeshGID->point ( GlobalID ).y() - MeshNeighbors->point ( *it ).y(), 2 )
+                        + std::pow ( MeshGID->point ( GlobalID ).z() - MeshNeighbors->point ( *it ).z(), 2 ) );
         r_max = ( r > r_max ) ? r : r_max;
     }
     return r_max;
@@ -501,8 +501,8 @@ bool RBFhtpVectorial<mesh_Type>::isInside (ID pointMarker, flagContainer_Type fl
 template <typename mesh_Type>
 double RBFhtpVectorial<mesh_Type>::rbf (double x1, double y1, double z1, double x2, double y2, double z2, double radius)
 {
-    double distance = sqrt ( pow (x1 - x2, 2) + pow (y1 - y2, 2) + pow (z1 - z2, 2) );
-    return pow (1 - distance / radius, 4) * (4 * distance / radius + 1);
+    double distance = std::sqrt ( std::pow (x1 - x2, 2) + std::pow (y1 - y2, 2) + std::pow (z1 - z2, 2) );
+    return std::pow (1 - distance / radius, 4) * (4 * distance / radius + 1);
 }
 
 template <typename mesh_Type>
