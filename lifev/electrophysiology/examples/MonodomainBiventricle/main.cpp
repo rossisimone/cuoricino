7394ed7efc82a7baabc0cedfa84823beece779aa
Toni Mikael Lassila <lassila@mathicsepc10.epfl.ch>
2013-09-11 12:01:41 +0200

    Created example MonodomainBiventricle

---
+++ lifev/electrophysiology/examples/MonodomainBiventricle/main.cpp
@@ -0,0 +1,506 @@
+//@HEADER
+/*
+ *******************************************************************************
+
+    Copyright (C) 2004, 2005, 2007 EPFL, Politecnico di Milano, INRIA
+    Copyright (C) 2010 EPFL, Politecnico di Milano, Emory University
+
+    This file is part of LifeV.
+
+    LifeV is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    LifeV is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with LifeV.  If not, see <http://www.gnu.org/licenses/>.
+
+ *******************************************************************************
+ */
+//@HEADER
+
+/*!
+    @file
+    @brief Electrophysiology benchmark of Niederer et al. 2011
+
+    @date 01−2013
+    @author Simone Rossi <simone.rossi@epfl.ch>
+
+    @contributor
+    @mantainer Simone Rossi <simone.rossi@epfl.ch>
+ */
+
+// Tell the compiler to ignore specific kind of warnings:
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+#include <Epetra_ConfigDefs.h>
+#ifdef EPETRA_MPI
+#include <mpi.h>
+#include <Epetra_MpiComm.h>
+#else
+#include <Epetra_SerialComm.h>
+#endif
+
+//Tell the compiler to restore the warning previously silented
+#pragma GCC diagnostic warning "-Wunused-variable"
+#pragma GCC diagnostic warning "-Wunused-parameter"
+
+
+
+#include <fstream>
+#include <string>
+
+#include <lifev/core/array/VectorSmall.hpp>
+
+#include <lifev/core/array/VectorEpetra.hpp>
+#include <lifev/core/array/MatrixEpetra.hpp>
+#include <lifev/core/array/MapEpetra.hpp>
+#include <lifev/core/mesh/MeshData.hpp>
+#include <lifev/core/mesh/MeshPartitioner.hpp>
+#include <lifev/core/filter/ExporterEnsight.hpp>
+#include <lifev/core/filter/ExporterHDF5.hpp>
+#include <lifev/core/filter/ExporterEmpty.hpp>
+
+#include <lifev/core/algorithm/LinearSolver.hpp>
+#include <lifev/electrophysiology/solver/ElectroETAMonodomainSolver.hpp>
+
+#include <lifev/core/filter/ExporterEnsight.hpp>
+#ifdef HAVE_HDF5
+#include <lifev/core/filter/ExporterHDF5.hpp>
+#endif
+#include <lifev/core/filter/ExporterEmpty.hpp>
+
+#include <lifev/electrophysiology/solver/IonicModels/IonicMinimalModel.hpp>
+#include <lifev/electrophysiology/solver/IonicModels/IonicLuoRudyI.hpp>
+#include <lifev/electrophysiology/solver/IonicModels/IonicTenTusscher06.hpp>
+
+#include <lifev/core/LifeV.hpp>
+
+#include <Teuchos_RCP.hpp>
+#include <Teuchos_ParameterList.hpp>
+#include "Teuchos_XMLParameterListHelpers.hpp"
+
+#include <sys/stat.h>
+
+using namespace LifeV;
+
+Real PacingProtocolMM ( const Real& t, const Real& x, const Real& y, const Real& z, const ID&   /*id*/)
+{
+
+    const int numberOfPacingSites = 3;
+
+    Real pacingSite_X[numberOfPacingSites]   = { 13.0, 15.2, 9.7 };
+    Real pacingSite_Y[numberOfPacingSites]   = { 6.5 ,  8.5, 6.6 };
+    Real pacingSite_Z[numberOfPacingSites]   = { 15.0, 18.2, 15. };
+    Real stimulusRadius[numberOfPacingSites] = { 0.15,  0.15,  0.1 };
+    Real stimulusValue = 50;
+
+    Real returnValue = 0;
+
+    for (int i=0; i < numberOfPacingSites; i++)
+    {
+        if ( std::abs( x - pacingSite_X[i] ) <= stimulusRadius[i] &&
+                std::abs( z - pacingSite_Z[i] ) <= stimulusRadius[i] &&
+                std::abs( y - pacingSite_Y[i] ) <= stimulusRadius[i] &&
+                t <= 2)
+        {
+            returnValue = stimulusValue;
+        }
+    }
+
+    return returnValue;
+
+}
+
+Real PacingProtocol ( const Real& t, const Real& x, const Real& y, const Real& z, const ID&   /*id*/)
+{
+
+    const int numberOfPacingSites = 3;
+
+    Real pacingSite_X[numberOfPacingSites]   = { 13.0, 15.2, 9.7 };
+    Real pacingSite_Y[numberOfPacingSites]   = { 6.5 ,  8.5, 6.6 };
+    Real pacingSite_Z[numberOfPacingSites]   = { 15.0, 18.2, 15. };
+    Real stimulusRadius[numberOfPacingSites] = { 0.15,  0.15,  0.1 };
+    Real stimulusValue = 50;
+
+    Real returnValue = 0;
+
+    for (int i=0; i < numberOfPacingSites; i++)
+    {
+        if ( std::abs( x - pacingSite_X[i] ) <= stimulusRadius[i] &&
+                std::abs( z - pacingSite_Z[i] ) <= stimulusRadius[i] &&
+                std::abs( y - pacingSite_Y[i] ) <= stimulusRadius[i] &&
+                t <= 2)
+        {
+            returnValue = stimulusValue;
+        }
+    }
+
+    return returnValue;
+
+}
+
+
+Int main ( Int argc, char** argv )
+{
+
+    //! Initializing Epetra communicator
+    MPI_Init (&argc, &argv);
+    boost::shared_ptr<Epetra_Comm>  Comm ( new Epetra_MpiComm (MPI_COMM_WORLD) );
+    if ( Comm->MyPID() == 0 )
+    {
+        cout << "% using MPI" << endl;
+    }
+
+    //*********************************************//
+    // creating output folder
+    //*********************************************//
+    GetPot commandLine ( argc, argv );
+    std::string problemFolder = commandLine.follow ( "Output", 2, "-o", "--output" );
+    // Create the problem folder
+    if ( problemFolder.compare ("./") )
+    {
+        problemFolder += "/";
+
+        if ( Comm->MyPID() == 0 )
+        {
+            mkdir ( problemFolder.c_str(), 0777 );
+        }
+    }
+
+    //********************************************//
+    // Starts the chronometer.                    //
+    //********************************************//
+
+    typedef RegionMesh<LinearTetra>                         mesh_Type;
+    typedef boost::function < Real (const Real& /*t*/,
+            const Real &   x,
+            const Real &   y,
+            const Real& /*z*/,
+            const ID&   /*i*/ ) >   function_Type;
+
+    typedef ElectroIonicModel	 ionicModel_Type;
+    typedef boost::shared_ptr<ionicModel_Type> ionicModelPtr_Type;
+    typedef ElectroETAMonodomainSolver< mesh_Type, ionicModel_Type >        monodomainSolver_Type;
+    typedef boost::shared_ptr< monodomainSolver_Type >  monodomainSolverPtr_Type;
+
+    typedef VectorEpetra				                             vector_Type;
+    typedef boost::shared_ptr<vector_Type>                           vectorPtr_Type;
+
+
+    LifeChrono chronoinitialsettings;
+
+    if ( Comm->MyPID() == 0 )
+        chronoinitialsettings.start();
+
+    //********************************************//
+    // Import parameters from an xml list. Use    //
+    // Teuchos to create a list from a given file //
+    // in the execution directory.                //
+    //********************************************//
+
+    if ( Comm->MyPID() == 0 )
+    {
+        std::cout << "Importing parameters list...";
+    }
+    Teuchos::ParameterList monodomainList = * ( Teuchos::getParametersFromXmlFile ( "MonodomainSolverParamList.xml" ) );
+    if ( Comm->MyPID() == 0 )
+    {
+        std::cout << " Done!" << endl;
+    }
+
+    std::string ionic_model( monodomainList.get ("ionic_model", "minimalModel") );
+    if ( Comm->MyPID() == 0 )
+    {
+        std::cout << "\nIonic_Model:" << ionic_model;
+    }
+
+
+
+    //********************************************//
+    // Creates a new model object representing the//
+    // model from Aliev and Panfilov 1996.  The   //
+    // model input are the parameters. Pass  the  //
+    // parameter list in the constructor          //
+    //********************************************//
+    if ( Comm->MyPID() == 0 )
+    {
+        std::cout << "\nBuilding Constructor for " << ionic_model << " Model with parameters ... ";
+    }
+    ionicModelPtr_Type  model;
+    if( ionic_model == "LuoRudyI" )
+    	model.reset( new IonicLuoRudyI() );
+    else if ( ionic_model == "TenTusscher06")
+    	model.reset(new IonicTenTusscher06() );
+    else
+        model.reset( new IonicMinimalModel() );
+
+    if ( Comm->MyPID() == 0 )
+    {
+        std::cout << " Done!" << endl;
+    }
+
+    model -> showMe();
+
+
+    //********************************************//
+    // In the parameter list we need to specify   //
+    // the mesh name and the mesh path.           //
+    //********************************************//
+    std::string meshName = monodomainList.get ("mesh_name", "lid16.mesh");
+    std::string meshPath = monodomainList.get ("mesh_path", "./");
+
+    //********************************************//
+    // We need the GetPot datafile for to setup   //
+    // the preconditioner.                        //
+    //********************************************//
+    GetPot command_line (argc, argv);
+    const string data_file_name = command_line.follow ("data", 2, "-f", "--file");
+    GetPot dataFile (data_file_name);
+
+    //********************************************//
+    // We create three solvers to solve with:     //
+    // 1) Operator Splitting method               //
+    // 2) Ionic Current Interpolation             //
+    // 3) State Variable Interpolation            //
+    //********************************************//
+    if ( Comm->MyPID() == 0 )
+    {
+        std::cout << "Building Monodomain Solvers... ";
+    }
+
+    monodomainSolverPtr_Type solver ( new monodomainSolver_Type ( meshName, meshPath, dataFile, model ) );
+    if ( Comm->MyPID() == 0 )
+    {
+        std::cout << " solver done... ";
+    }
+
+
+    //********************************************//
+    // Setting up the initial condition form      //
+    // a given function.                          //
+    //********************************************//
+    if ( Comm->MyPID() == 0 )
+    {
+        cout << "\nInitializing potential and gating variables:  " ;
+    }
+
+    // Initial pacing
+    solver -> initializePotential();
+    solver -> initializeAppliedCurrent();
+    solver -> setInitialConditions();
+
+    function_Type stimulus;
+    if(ionic_model == "MinimalModel" )
+        stimulus = &PacingProtocolMM;
+    else
+        stimulus = &PacingProtocol;
+    solver -> setAppliedCurrentFromFunction(stimulus, 0.0);
+
+    if ( Comm->MyPID() == 0 )
+    {
+        cout << "Done! \n" ;
+    }
+
+    //********************************************//
+    // Setting up the time data                   //
+    //********************************************//
+    solver -> setParameters ( monodomainList );
+
+    //********************************************//
+    // Create a fiber direction                   //
+    //********************************************//
+    if ( Comm->MyPID() == 0 )
+    {
+        cout << "\nSetting fibers:  " ;
+    }
+
+    /*
+    VectorSmall<3> fibers;
+    fibers[0]=0.0;
+    fibers[1]=0.0;
+    fibers[2]=1.0;
+    solver -> setupFibers( fibers );
+    */
+
+    boost::shared_ptr<FESpace< mesh_Type, MapEpetra > > Space3D
+    ( new FESpace< mesh_Type, MapEpetra > ( solver -> localMeshPtr(), "P1", 3, solver -> commPtr() ) );
+
+    boost::shared_ptr<VectorEpetra> fiber ( new VectorEpetra ( Space3D -> map() ) );
+    std::string nm = monodomainList.get("fiber_file","FiberDirection") ;
+    HeartUtility::importFibers( fiber, nm, solver -> localMeshPtr() );
+
+    solver -> setFiberPtr(fiber);
+
+    if ( Comm->MyPID() == 0 )
+    {
+        cout << "Done! \n" ;
+    }
+
+    //********************************************//
+    // Create the global matrix: mass + stiffness //
+    //********************************************//
+    if ( Comm->MyPID() == 0 )
+    {
+        cout << "\nSetup operators:  " ;
+    }
+
+    bool lumpedMass = monodomainList.get ("LumpedMass", true);
+    if( lumpedMass)
+        solver -> setupLumpedMassMatrix();
+    else
+        solver -> setupMassMatrix();
+
+
+    solver -> setupStiffnessMatrix( solver -> diffusionTensor() );
+    solver -> setupGlobalMatrix();
+    if ( Comm->MyPID() == 0 )
+    {
+        cout << "Done! \n" ;
+    }
+
+    //********************************************//
+    // Creating exporters to save the solution    //
+    //********************************************//
+    ExporterHDF5< RegionMesh <LinearTetra> > exporter;
+
+    solver -> setupExporter ( exporter, monodomainList.get ("OutputFile", "Solution") );
+    exporter.setPostDir(problemFolder);
+    solver -> exportSolution ( exporter, 0);
+
+    //********************************************//
+    // Activation time						      //
+    //********************************************//
+    vectorPtr_Type activationTimeVector( new vector_Type( solver -> potentialPtr() -> map() ) );
+    *activationTimeVector = -1.0;
+
+    ExporterHDF5< RegionMesh <LinearTetra> > activationTimeExporter;
+    activationTimeExporter.setMeshProcId(solver -> localMeshPtr(), solver -> commPtr() ->MyPID());
+    activationTimeExporter.addVariable(ExporterData<mesh_Type>::ScalarField, "Activation Time",
+            solver -> feSpacePtr(), activationTimeVector, UInt(0));
+    activationTimeExporter.setPrefix("ActivationTime");
+    activationTimeExporter.setPostDir(problemFolder);
+
+    //********************************************//
+    // Solving the system                         //
+    //********************************************//
+    if ( Comm->MyPID() == 0 )
+    {
+        cout << "\nstart solving:  " ;
+    }
+
+    Real dt = monodomainList.get ("timeStep", 0.1);
+    Real TF = monodomainList.get ("endTime", 150.0);
+    Int iter = monodomainList.get ("saveStep", 1.0) / dt;
+    Int k(0);
+
+    Real timeReac = 0.0;
+    Real timeDiff = 0.0;
+    Real timeReacDiff = 0.0;
+    LifeChrono chrono;
+
+    std::string solutionMethod = monodomainList.get ("solutionMethod", "splitting");
+
+
+    for ( Real t = 0.0; t < TF; )
+    {
+        solver -> setAppliedCurrentFromFunction ( stimulus, t );
+
+        if( solutionMethod == "splitting" )
+        {
+            chrono.reset();
+            chrono.start();
+            solver->solveOneReactionStepFE( );
+            chrono.stop();
+
+            timeReac += chrono.globalDiff( *Comm );
+
+            (*solver->rhsPtrUnique()) *= 0.0;
+            solver->updateRhs();
+
+            chrono.reset();
+            chrono.start();
+            solver->solveOneDiffusionStepBE();
+            chrono.stop();
+            timeDiff += chrono.globalDiff( *Comm );
+        }
+        else if( solutionMethod == "ICI" )
+        {
+            chrono.reset();
+            chrono.start();
+            solver -> solveOneStepGatingVariablesFE();
+            solver -> solveOneICIStep();
+            chrono.stop();
+            timeReacDiff += chrono.globalDiff( *Comm );
+        }
+        else if( solutionMethod == "SVI" )
+        {
+            chrono.reset();
+            chrono.start();
+            solver -> solveOneStepGatingVariablesFE();
+            solver -> solveOneSVIStep();
+            chrono.stop();
+            timeReacDiff += chrono.globalDiff( *Comm );
+        }
+
+        //register activation time
+        k++;
+        t = t + dt;
+        solver -> registerActivationTime(*activationTimeVector, t, 1.0);
+
+        if( k % iter == 0 )
+        {
+            solver -> exportSolution (exporter, t);
+        }
+        if ( Comm->MyPID() == 0 )
+            std::cout<<"\n\n\nActual time : "<<t<<std::endl<<std::endl<<std::endl;
+    }
+
+    Real normSolution = ( ( solver -> globalSolution().at (0) )->norm2());
+    if ( Comm->MyPID() == 0 )
+        std::cout << "2-norm of potential solution: " << normSolution << std::endl;
+
+    exporter.closeFile();
+    activationTimeExporter.postProcess(0);
+    activationTimeExporter.closeFile();
+
+    if ( Comm->MyPID() == 0 )
+        std::cout << "Exporting fibers: " << std::endl;
+
+    //********************************************//
+    // Saving Fiber direction to file             //
+    //********************************************//
+    solver -> exportFiberDirection();
+
+
+    if ( Comm->MyPID() == 0 )
+    {
+        chronoinitialsettings.stop();
+        std::cout << "\n\n\nTotal lapsed time : " << chronoinitialsettings.diff() << std::endl;
+        if( solutionMethod == "splitting" )
+        {
+            std::cout<<"Diffusion time : "<<timeDiff<<std::endl;
+            std::cout<<"Reaction time : "<<timeReac<<std::endl;
+        }
+        else if( solutionMethod == "ICI" )
+        {
+            std::cout<<"Solution time : "<<timeReacDiff<<std::endl;
+        }
+        else if( solutionMethod == "SVI" )
+        {
+            std::cout<<"Solution time : "<<timeReacDiff<<std::endl;
+        }
+
+        std::cout << "\n\nThank you for using ETA_MonodomainSolver.\nI hope to meet you again soon!\n All the best for your simulation :P\n  " ;
+    }
+    MPI_Barrier (MPI_COMM_WORLD);
+    MPI_Finalize();
+
+    return ( EXIT_SUCCESS );
+}
