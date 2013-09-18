9dde061271a963a6a04e3fcedcb63f989a3c38f6
SimonePalamara <palamara.simone@gmail.com>
2013-09-18 10:40:45 +0200

    Implementation of HodgkinHuxley model, with test0D and test3D

---
+++ lifev/electrophysiology/solver/IonicModels/IonicHodgkinHuxley.cpp
@@ -0,0 +1,244 @@
+//@HEADER
+/*
+*******************************************************************************
+
+    Copyright (C) 2004, 2005, 2007 EPFL, Politecnico di Milano, INRIA
+    Copyright (C) 2010 EPFL, Politecnico di Milano, Emory University
+
+    This file is part of LifeV.
+
+    LifeV is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    LifeV is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with LifeV.  If not, see <http://www.gnu.org/licenses/>.
+
+*******************************************************************************
+*/
+//@HEADER
+
+/*!
+  @file
+  @brief Mean-field cross-bridge model by Negroni and Lascano 1996.
+  @date 01-2013
+  @author Simone Rossi <simone.rossi@epfl.ch>
+
+  @contributors
+  @mantainer Simone Rossi <simone.rossi@epfl.ch>
+  @last update 01-2013
+ */
+
+
+#include <lifev/electrophysiology/solver/IonicModels/IonicHodgkinHuxley.hpp>
+
+
+namespace LifeV
+{
+
+// ===================================================
+//! Constructors
+// ===================================================
+//IonicMinimalModel::IonicMinimalModel()    :
+//      super       ( 4     ),
+//      M_uo        ( 0.    ),
+//      M_uu        ( 1.61  ),
+//      M_tetav     ( 0.3   ),
+//      M_tetaw     ( 0.13  ),
+//      M_tetavm    ( 0.1   ),
+//      M_tetao     ( 0.005 ),
+//      M_tauv1     ( 80.0  ),
+//      M_tauv2     ( 1.4506),
+//      M_tauvp     ( 1.4506),
+//      M_tauw1     ( 70.0  ),
+//      M_tauw2     ( 8.0   ),
+//      M_kw        ( 200.0 ),
+//      M_uw        ( 0.016 ),
+//      M_tauwp     ( 280.0 ),
+//      M_taufi     ( 0.078 ),
+//      M_tauo1     ( 410.0 ),
+//      M_tauo2     ( 7.0   ),
+//      M_tauso1    ( 91.0  ),
+//      M_tauso2    ( 0.8   ),
+//      M_kso       ( 2.1   ),
+//      M_uso       ( 0.6   ),
+//      M_taus1     ( 2.7342),
+//      M_taus2     ( 4.0   ),
+//      M_ks        ( 2.0994),
+//      M_us        ( 0.9087),
+//      M_tausi     ( 3.3849),
+//      M_tauwinf   ( 0.01  ),
+//      M_winfstar  ( 0.5   )
+//{
+//}
+
+
+IonicHodgkinHuxley::IonicHodgkinHuxley()  :
+super       ( 4,3 ),
+M_gNa(120.0),
+M_gK(36.0),
+M_gL(0.3),
+M_vNa(115.0),
+M_vK(-12.0),
+M_vL(10.6)
+{
+	M_restingConditions.at(0) = 0.0;
+	M_restingConditions.at(1) = 0.052932485257250;
+	M_restingConditions.at(2) = 0.317676914060697;
+	M_restingConditions.at(3) = 0.596120753508460;
+
+}
+
+IonicHodgkinHuxley::IonicHodgkinHuxley ( Teuchos::ParameterList& parameterList     )   :
+    super       ( 4,3 )
+{
+    M_gNa        =  parameterList.get ("gNa",      120.0     );
+    M_gK        =  parameterList.get ("gK",     36.0     );
+    M_gL        =  parameterList.get ("gL",      0.3     );
+    M_vNa        =  parameterList.get ("vNa",      115.0     );
+    M_vK        =  parameterList.get ("vK",      -12.0     );
+    M_vL        =  parameterList.get ("vL",      10.6     );
+
+}
+
+IonicHodgkinHuxley::IonicHodgkinHuxley ( const IonicHodgkinHuxley& model )
+{
+
+    M_gNa        =  model.M_gNa;
+    M_gK        =  model.M_gK;
+    M_gL        =  model.M_gL;
+    M_vNa        =  model.M_vNa;
+    M_vK        =  model.M_vK;
+    M_vL        =  model.M_vL;
+
+
+    M_numberOfEquations = model.M_numberOfEquations;
+    M_restingConditions = model.M_restingConditions;
+
+}
+
+// ===================================================
+//! Operator
+// ===================================================
+IonicHodgkinHuxley& IonicHodgkinHuxley::operator= ( const IonicHodgkinHuxley& model )
+{
+    M_gNa        =  model.M_gNa;
+    M_gK        =  model.M_gK;
+    M_gL        =  model.M_gL;
+    M_vNa        =  model.M_vNa;
+    M_vK        =  model.M_vK;
+    M_vL        =  model.M_vL;
+
+    M_numberOfEquations = model.M_numberOfEquations;
+    M_restingConditions = model.M_restingConditions;
+
+
+    return      *this;
+}
+
+
+// ===================================================
+//! Methods
+// ===================================================
+void IonicHodgkinHuxley::computeGatingRhs ( const   std::vector<Real>&  v,
+                                     std::vector<Real>& rhs )
+{
+
+    Real V = v[0];
+    Real M = v[1];
+    Real N = v[2];
+    Real H = v[3];
+
+    Real alpham=0.1*(25.-V)/(std::exp((25.-V)/10.)-1.);
+    Real betam=4.*exp(-V/18.0);
+    Real alphah=0.07*exp(-V/20.);
+    Real betah=1.0/(exp((30.-V)/10.)+1.);
+    Real alphan=0.01*(10.-V)/(exp((10.-V)/10.)-1.);
+    Real betan=0.125*exp(-V/80.0);
+
+    rhs[0] = alpham*(1-M)-betam*M;
+    rhs[1] = alphan*(1-N)-betan*N;
+    rhs[2] = alphah*(1-H)-betah*H;
+}
+
+void IonicHodgkinHuxley::computeRhs ( const   std::vector<Real>&  v,
+                                     std::vector<Real>& rhs )
+{
+
+    Real V = v[0];
+    Real M = v[1];
+    Real N = v[2];
+    Real H = v[3];
+
+    Real alpham=0.1*(25.-V)/(exp((25.-V)/10.)-1.);
+    Real betam=4.*exp(-V/18.0);
+    Real alphah=0.07*exp(-V/20.);
+    Real betah=1.0/(exp((30.-V)/10.)+1.);
+    Real alphan=0.01*(10.-V)/(exp((10.-V)/10.)-1.);
+    Real betan=0.125*exp(-V/80.0);
+
+    rhs[0] = -M_gK*N*N*N*N*(V-M_vK)-M_gNa*M*M*M*H*(V-M_vNa)-M_gL*(V-M_vL)+M_appliedCurrent;
+    rhs[1] = alpham*(1-M)-betam*M;
+    rhs[2] = alphan*(1-N)-betan*N;
+    rhs[3] = alphah*(1-H)-betah*H;
+}
+
+
+Real IonicHodgkinHuxley::computeLocalPotentialRhs ( const std::vector<Real>& v )
+{
+    Real dPotential (0.0);
+
+    Real V = v[0];
+    Real M = v[1];
+    Real N = v[2];
+    Real H = v[3];
+
+
+    dPotential = -M_gK*N*N*N*N*(V-M_vK)-M_gNa*M*M*M*H*(V-M_vNa)-M_gL*(V-M_vL)+M_appliedCurrent;
+
+    return dPotential;
+}
+
+void IonicHodgkinHuxley::computeGatingVariablesWithRushLarsen ( std::vector<Real>& v, const Real dt )
+{
+    Real V = v[0];
+    Real M = v[1];
+    Real N = v[2];
+    Real H = v[3];
+
+    Real alpham=0.1*(25.-V)/(exp((25.-V)/10.)-1.);
+    Real betam=4.*exp(-V/18.0);
+    Real alphah=0.07*exp(-V/20.);
+    Real betah=1.0/(exp((30.-V)/10.)+1.);
+    Real alphan=0.01*(10.-V)/(exp((10.-V)/10.)-1.);
+    Real betan=0.125*exp(-V/80.0);
+
+    Real taum=alpham + betam;
+    Real taun=alphan + betan;
+    Real tauh=alphah + betah;
+
+    Real mInf=alpham /(taum);
+    Real nInf=alphan /(taun);
+    Real hInf=alphah /(tauh);
+
+    v[1]=mInf+(v[1]-mInf)*exp(-dt/taum);
+    v[2]=nInf+(v[2]-nInf)*exp(-dt/taun);
+    v[3]=hInf+(v[3]-hInf)*exp(-dt/tauh);
+
+}
+
+void IonicHodgkinHuxley::showMe()
+{
+
+    std::cout << "\n\tHi, I'm the minimal model\n\t See you soon\n\n";
+}
+
+
+}
+
---
+++ lifev/electrophysiology/solver/IonicModels/IonicHodgkinHuxley.hpp
@@ -0,0 +1,209 @@
+//@HEADER
+/*
+*******************************************************************************
+
+    Copyright (C) 2004, 2005, 2007 EPFL, Politecnico di Milano, INRIA
+    Copyright (C) 2010 EPFL, Politecnico di Milano, Emory University
+
+    This file is part of LifeV.
+
+    LifeV is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    LifeV is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with LifeV.  If not, see <http://www.gnu.org/licenses/>.
+
+*******************************************************************************
+*/
+//@HEADER
+
+/*!
+  @file
+  @brief Mean-field cross-bridge model by Negroni and Lascano 1996.
+  @date 01-2013
+  @author Simone Rossi <simone.rossi@epfl.ch>
+
+  @contributors
+  @mantainer Simone Rossi <simone.rossi@epfl.ch>
+  @last update 01-2013
+ */
+
+
+#ifndef _IONICHODGKINHUXLEY_H_
+#define _IONICHODGKINHUXLEY_H_
+
+#include <lifev/electrophysiology/solver/IonicModels/ElectroIonicModel.hpp>
+
+#include <Teuchos_RCP.hpp>
+#include <Teuchos_ParameterList.hpp>
+#include "Teuchos_XMLParameterListHelpers.hpp"
+
+namespace LifeV
+{
+//! XbModel - This class implements a mean field model.
+
+class IonicHodgkinHuxley : public virtual ElectroIonicModel
+{
+
+public:
+    //! @name Type definitions
+    //@{
+    typedef ElectroIonicModel                         super;
+    typedef boost::shared_ptr<VectorEpetra>         vectorPtr_Type;
+    typedef boost::shared_ptr<VectorElemental>  elvecPtr_Type;
+    typedef RegionMesh<LinearTetra>                 mesh_Type;
+    //@}
+
+
+
+    //! @name Constructors & Destructor
+    //@{
+
+    //! Constructor
+    IonicHodgkinHuxley();
+
+    /*!
+     * @param Epetra communicator
+     * @param list of parameters in an xml file
+     */
+    IonicHodgkinHuxley ( Teuchos::ParameterList& parameterList );
+
+    /*!
+     * @param IonicHodgkinHuxley object
+     */
+    IonicHodgkinHuxley ( const IonicHodgkinHuxley& model );
+    //! Destructor
+    virtual ~IonicHodgkinHuxley() {}
+
+    //@}
+
+    //! @name Overloads
+    //@{
+
+    IonicHodgkinHuxley& operator= ( const IonicHodgkinHuxley& model );
+
+    //@}
+
+    //! @name Setters and getters
+    //@{
+
+    //parameters getters and setters
+    inline const Real& gNa()             const
+    {
+        return M_gNa;
+    }
+
+    inline const Real& gK()             const
+    {
+        return M_gK;
+    }
+
+    inline const Real& gL()             const
+    {
+        return M_gL;
+    }
+
+
+    inline const Real& vL()             const
+    {
+        return M_vL;
+    }
+
+    inline const Real& vK()             const
+    {
+        return M_vK;
+    }
+
+    inline const Real& vNa()             const
+    {
+        return M_vNa;
+    }
+
+
+
+
+    inline void setgNa           ( const Real& p )
+    {
+        this->M_gNa        = p;
+    }
+
+    inline void setgK           ( const Real& p )
+    {
+        this->M_gK        = p;
+    }
+
+    inline void setgL           ( const Real& p )
+    {
+        this->M_gL        = p;
+    }
+
+    inline void setvL           ( const Real& p )
+    {
+        this->M_vL        = p;
+    }
+
+    inline void setvNa          ( const Real& p )
+    {
+        this->M_vNa        = p;
+    }
+
+    inline void setvK           ( const Real& p )
+    {
+        this->M_vK         = p;
+    }
+
+
+    //inline const short int& Size() const { return M_numberOfEquations; }
+    //@}
+
+
+    //Compute the rhs on a single node or for the 0D case
+    void computeGatingRhs ( const std::vector<Real>& v, std::vector<Real>& rhs);
+
+    void computeRhs ( const std::vector<Real>& v, std::vector<Real>& rhs);
+
+    // compute the rhs with state variable interpolation
+    Real computeLocalPotentialRhs ( const std::vector<Real>& v );
+
+    void computeGatingVariablesWithRushLarsen ( std::vector<Real>& v, const Real dt );
+
+
+    //! Display information about the model
+    void showMe();
+
+    //! Solves the ionic model
+    //virtual void solveXbModel( const vector_Type& Calcium,
+    //                           const Real timeStep )=0;
+    //@}
+
+private:
+    //! Model Parameters
+
+    //! Chemical kinetics parameters
+    Real M_gNa;
+    Real M_gK;
+    Real M_gL;
+    Real M_vNa;
+    Real M_vK;
+    Real M_vL;
+
+
+    //! Xb states == equivalent to the number of equations
+    //short int M_numberOfEquations;
+
+    //@}
+
+}; // class IonicMinimalModel
+
+
+
+}
+
+#endif
