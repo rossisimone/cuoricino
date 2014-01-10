c845e99f34207e06809d6b2ad696314dae7d2e41
Toni Mikael Lassila <lassila@mathicsepc10.epfl.ch>
2014-01-10 14:55:50 +0100

    Working version of lvfibers

--- lifev/toolkit/lvfibers/lvfibers.cpp
+++ lifev/toolkit/lvfibers/lvfibers.cpp
@@ -34,10 +34,7 @@
     @mantainer
  */
 
-#pragma GCC diagnostic ignored "-Wunused-variable"
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-
-#include <Epetra_ConfigDefs.h>
+//#include <Epetra_ConfigDefs.h>
 #ifdef EPETRA_MPI
 #include <mpi.h>
 #include <Epetra_MpiComm.h>
@@ -45,109 +42,37 @@
 #include <Epetra_SerialComm.h>
 #endif
 
-#pragma GCC diagnostic warning "-Wunused-variable"
-#pragma GCC diagnostic warning "-Wunused-parameter"
-
-
-// ---------------------------------------------------------------
-// We include then the required headers from LifeV. First of all,
-// the definition file and mesh related files. We also include
-// the MatrixEpetra since this is the kind of object that we want
-// to assemble.
-// ---------------------------------------------------------------
-
-#include <lifev/core/LifeV.hpp>
-
-#include <lifev/core/mesh/MeshPartitioner.hpp>
-#include <lifev/core/mesh/RegionMesh3DStructured.hpp>
-#include <lifev/core/mesh/RegionMesh.hpp>
-
-#include <lifev/core/array/MatrixEpetra.hpp>
-
-// ---------------------------------------------------------------
-// In order to use the ETA framework, a special version of the
-// FESpace structure must be used. It is called ETFESpace and
-// has basically the same role as the FESpace.
-// ---------------------------------------------------------------
-
-#include <lifev/eta/fem/ETFESpace.hpp>
-
-
-// ---------------------------------------------------------------
-// The most important file to include is the Integrate.hpp file
-// which contains all the definitions required to perform the
-// different integrations.
-// ---------------------------------------------------------------
-
-#include <lifev/eta/expression/Integrate.hpp>
-
-
-// ---------------------------------------------------------------
-// Finally, we include shared pointer from boost since we use
-// them explicitly in this tutorial.
-// ---------------------------------------------------------------
-
 #include <boost/shared_ptr.hpp>
 
-#include <lifev/core/util/HeartUtility.hpp>
-#include <lifev/core/mesh/MeshUtility.hpp>
-#include <lifev/core/mesh/MeshTransformer.hpp>
-#include <lifev/core/mesh/MeshLoadingUtility.hpp>
-
-#include <Teuchos_RCP.hpp>
 #include <Teuchos_ParameterList.hpp>
-#include "Teuchos_XMLParameterListHelpers.hpp"
 
-#include <lifev/bc_interface/3D/bc/BCInterface3D.hpp>
-#include <lifev/core/algorithm/LinearSolver.hpp>
-#include <lifev/core/algorithm/Preconditioner.hpp>
-#include <lifev/core/fem/BCManage.hpp>
+#include <sys/stat.h>
 
-#include <lifev/core/filter/ExporterEnsight.hpp>
+#include <lifev/core/mesh/MeshLoadingUtility.hpp>
+#include <lifev/core/fem/GradientRecovery.hpp>
+
 #ifdef HAVE_HDF5
 #include <lifev/core/filter/ExporterHDF5.hpp>
 #endif
-#include <lifev/core/filter/ExporterEmpty.hpp>
 
-#include <sys/stat.h>
-#include <lifev/core/fem/GradientRecovery.hpp>
-// ---------------------------------------------------------------
-// As usual, we work in the LifeV namespace. For clarity, we also
-// make two typedefs for the mesh type and matrix type.
-// ---------------------------------------------------------------
+#include <lifev/bc_interface/3D/bc/BCInterface3D.hpp>
 
 using namespace LifeV;
 
-typedef RegionMesh<LinearTetra> mesh_Type;
-typedef boost::shared_ptr< mesh_Type > meshPtr_Type;
-typedef MatrixEpetra<Real> matrix_Type;
-typedef boost::shared_ptr< matrix_Type > matrixPtr_Type;
-typedef VectorEpetra vector_Type;
-typedef boost::shared_ptr< vector_Type > vectorPtr_Type;
-typedef BCHandler                                          bc_Type;
-typedef boost::shared_ptr< bc_Type >                       bcPtr_Type;
-typedef  StructuralOperator< RegionMesh<LinearTetra> >      physicalSolver_Type;
-typedef BCInterface3D< bc_Type, physicalSolver_Type >              bcInterface_Type;
-typedef boost::shared_ptr< bcInterface_Type >              bcInterfacePtr_Type;
-typedef MeshUtility::MeshTransformer<mesh_Type>                         meshTransformer_Type;
+typedef RegionMesh<LinearTetra>                        mesh_Type;
+typedef boost::shared_ptr< mesh_Type >                 meshPtr_Type;
+typedef MatrixEpetra<Real>                             matrix_Type;
+typedef boost::shared_ptr< matrix_Type >               matrixPtr_Type;
+typedef VectorEpetra                                   vector_Type;
+typedef boost::shared_ptr< vector_Type >               vectorPtr_Type;
+typedef BCHandler                                      bc_Type;
+typedef boost::shared_ptr< bc_Type >                   bcPtr_Type;
+typedef  StructuralOperator< RegionMesh<LinearTetra> > physicalSolver_Type;
+typedef BCInterface3D< bc_Type, physicalSolver_Type >  bcInterface_Type;
+typedef boost::shared_ptr< bcInterface_Type >          bcInterfacePtr_Type;
+typedef MeshUtility::MeshTransformer<mesh_Type>        meshTransformer_Type;
 
 
-// ---------------------------------------------------------------
-// We start the programm by the definition of the communicator
-// (as usual) depending on whether MPI is available or not. We
-// also define a boolean to allow only one process to display
-// messages.
-// ---------------------------------------------------------------
-
-Real bcZero (const Real& /*t*/, const Real&  /*X*/, const Real& /*Y*/, const Real& /*Z*/, const ID& /*i*/)
-{
-    return  0.;
-}
-
-Real bcOne (const Real& /*t*/, const Real&  /*X*/, const Real& /*Y*/, const Real& /*Z*/, const ID& /*i*/)
-{
-    return  1.0;
-}
 
 int main ( int argc, char** argv )
 {
@@ -194,19 +119,6 @@ int main ( int argc, char** argv )
     meshPtr_Type meshPart (new mesh_Type ( Comm ) );
     MeshUtility::fillWithMesh (meshPart, meshName, problemFolder);
 
-    // ---------------------------------------------------------------
-    // We define now the ETFESpace that we need for the assembly.
-    // Remark that we use a shared pointer because other structures
-    // will require this ETFESpace to be alive. We can also observe
-    // that the ETFESpace has more template parameters than the
-    // classical FESpace (this is the main difference). The 3
-    // indicates that the problem is in 3D while the 1 indicate that
-    // the unknown is scalar.
-    //
-    // After having constructed the ETFESpace, we display the number
-    // of degrees of freedom of the problem.
-    // ---------------------------------------------------------------
-
     boost::shared_ptr<ETFESpace< mesh_Type, MapEpetra, 3, 1 > > uSpace
     ( new ETFESpace< mesh_Type, MapEpetra, 3, 1 > (meshPart, &feTetraP1, Comm) );
     boost::shared_ptr<FESpace< mesh_Type, MapEpetra > > uFESpace
@@ -233,10 +145,12 @@ int main ( int argc, char** argv )
         >> systemMatrix;
     }
 
+    systemMatrix->globalAssemble();
+
     //-----------------------
     //  Boundary conditions
     GetPot command_line (argc, argv);
-    const string data_file_name = command_line.follow ("FiberGenerationPreconditioner ", 2, "-p", "--prec");
+    const string data_file_name = command_line.follow ("FiberGenerationPreconditioner", 2, "-p", "--prec");
 
     GetPot dataFile (data_file_name);
     bcInterfacePtr_Type                     BC ( new bcInterface_Type() );
@@ -267,11 +181,7 @@ int main ( int argc, char** argv )
     *rhs *= 0.0;
     rhs -> globalAssemble();
 
-
     bcManage ( *systemMatrix, *rhs, *uSpace->mesh(), uSpace->dof(), *BC -> handler(), uFESpace->feBd(), 1.0, 0.0 );
-    systemMatrix->globalAssemble();
-
-    systemMatrix->spy("matrix");
 
     linearSolver.setOperator (systemMatrix);
     vectorPtr_Type solution ( new vector_Type ( uFESpace -> map() ) );
@@ -280,28 +190,12 @@ int main ( int argc, char** argv )
     vectorPtr_Type sy (new vector_Type ( uSpace -> map() ) );
     vectorPtr_Type sz (new vector_Type ( uSpace -> map() ) );
 
-
-    ExporterHDF5< RegionMesh <LinearTetra> > exporter;
-    exporter.setMeshProcId ( meshPart, Comm->MyPID() );
-    exporter.setPrefix ("laplacian");
-    exporter.setPostDir (problemFolder);
-    exporter.addVariable ( ExporterData<mesh_Type>::ScalarField,  "potential", uFESpace,
-                           solution, UInt (0) );
-    exporter.addVariable ( ExporterData<mesh_Type>::ScalarField,  "sx", uFESpace,
-                           sx, UInt (0) );
-    exporter.addVariable ( ExporterData<mesh_Type>::ScalarField,  "sy", uFESpace,
-                           sy, UInt (0) );
-    exporter.addVariable ( ExporterData<mesh_Type>::ScalarField,  "sz", uFESpace,
-                           sz, UInt (0) );
-
     linearSolver.setRightHandSide (rhs);
     linearSolver.solve (solution);
 
     *sx = GradientRecovery::ZZGradient (uSpace, *solution, 0);
     *sy = GradientRecovery::ZZGradient (uSpace, *solution, 1);
     *sz = GradientRecovery::ZZGradient (uSpace, *solution, 2);
-
-    exporter.postProcess ( 0 );
 
     vectorPtr_Type rbSheet ( new vector_Type ( sFESpace -> map() ) );
     vectorPtr_Type rbFiber ( new vector_Type ( sFESpace -> map() ) );
@@ -357,8 +251,8 @@ int main ( int argc, char** argv )
 
         Real scalarp;
         scalarp = (*projection) [i] * (*rbFiber) [i]
-                                                  + (*projection) [j] * (*rbFiber) [j]
-                                                                                    + (*projection) [k] * (*rbFiber) [k];
+                   + (*projection) [j] * (*rbFiber) [j]
+                   + (*projection) [k] * (*rbFiber) [k];
 
         if ( scalarp == 1.0 )
         {
@@ -369,23 +263,8 @@ int main ( int argc, char** argv )
 
     }
 
-
-    ExporterHDF5< mesh_Type > exporter2;
-    exporter2.setMeshProcId ( meshPart, Comm -> MyPID() );
-    exporter2.setPrefix ("sheets");
-    exporter2.setPostDir (problemFolder);
-    exporter2.addVariable ( ExporterData<mesh_Type>::VectorField,  "sheet", sFESpace, rbSheet, UInt (0) );
-    exporter2.addVariable ( ExporterData<mesh_Type>::VectorField,  "fiber", sFESpace, rbFiber, UInt (0) );
-    exporter2.addVariable ( ExporterData<mesh_Type>::VectorField,  "centerline", sFESpace, cl, UInt (0) );
-    exporter2.addVariable ( ExporterData<mesh_Type>::VectorField,  "projection", sFESpace, projection, UInt (0) );
-    exporter2.postProcess (0);
-
-
     HeartUtility::normalize (*rbFiber);
     HeartUtility::normalize (*projection);
-    exporter2.postProcess (1);
-
-
 
     for ( int l (0); l < d; l++)
     {
@@ -393,7 +272,7 @@ int main ( int argc, char** argv )
         int j = (*rbSheet).blockMap().GID (l + d);
         int k = (*rbSheet).blockMap().GID (l + 2 * d);
 
-        //fiber =  sheet x projection
+        // fiber =  sheet x projection
         (*rbFiber) [i] = (*rbSheet) [j]  * (*projection) [k] - (*rbSheet) [k]  * (*projection) [j];
         (*rbFiber) [j] = (*rbSheet) [k]  * (*projection) [i] - (*rbSheet) [i]  * (*projection) [k];
         (*rbFiber) [k] = (*rbSheet) [i]  * (*projection) [j] - (*rbSheet) [j]  * (*projection) [i];
@@ -401,7 +280,6 @@ int main ( int argc, char** argv )
 
     HeartUtility::normalize (*rbFiber);
 
-    exporter2.postProcess (2);
     Real epi_angle = parameterList.get ("epi_angle", -60.0);
     Real endo_angle = parameterList.get ("endo_angle", 60.0);
 
@@ -413,15 +291,15 @@ int main ( int argc, char** argv )
 
         Real scalarp;
         scalarp = (*projection) [i] * (*rbFiber) [i]
-                                                  + (*projection) [j] * (*rbFiber) [j]
-                                                                                    + (*projection) [k] * (*rbFiber) [k];
+                   + (*projection) [j] * (*rbFiber) [j]
+                   + (*projection) [k] * (*rbFiber) [k];
 
 
         Real p = 3.14159265358979;
         Real teta1 = p * epi_angle / 180;
         Real teta2 = p * endo_angle / 180;
         Real m = (teta1 - teta2 );
-        Real q = teta2;// - m * (*solution)[i];
+        Real q = teta2;
         Real teta;
 
         teta = m * (*solution) [i] + q;
@@ -475,12 +353,6 @@ int main ( int argc, char** argv )
         (*rbFiber) [k] = R31 * f01 + R32 * f02 + R33 * f03;
     }
 
-    exporter2.postProcess (3);
-    exporter2.closeFile();
-
-    exporter.postProcess (1);
-    exporter.closeFile();
-
     ExporterHDF5< mesh_Type > exporterFibers;
     exporterFibers.setMeshProcId ( meshPart, Comm -> MyPID() );
     exporterFibers.setPostDir (problemFolder);
@@ -498,16 +370,9 @@ int main ( int argc, char** argv )
     exporterSheets.postProcess (0);
     exporterSheets.closeFile();
 
-
-    // ---------------------------------------------------------------
-    // We finalize the MPI session if MPI was used
-    // ---------------------------------------------------------------
-
 #ifdef HAVE_MPI
     MPI_Finalize();
 #endif
-
-
 
 
 }
